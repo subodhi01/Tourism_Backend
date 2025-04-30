ed9098e213d4b674ba17a9ce648640ac41d548c5
oshiyz <oshadhadahanayaka0504@gmail.com>
2025-04-30 21:48:51 +0530

    COMMIT

--- obj/Debug/net9.0/TourismGalle.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismGalle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc191716dfed63eeb9310ed334cedcf4e2ad76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14efdfe6942579f59cf2109c5872cba7cf39d16b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
