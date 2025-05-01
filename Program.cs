1aad32ed2169f8656e0d9d1e4b7b8885ddbe2275
Subo <subodhinisanka@gmail.com>
2025-05-01 17:49:45 +0530

    AllowFrontEnd

--- Program.cs
+++ Program.cs
@@ -1,93 +1,41 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using TourismGalle.Services;
-//using TourismGalle.Models;
-//using TourismGalle.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add CORS policy
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAllOrigins",
-//        builder =>
-//        {
-//            builder.AllowAnyOrigin()
-//                   .AllowAnyMethod()
-//                   .AllowAnyHeader();
-//        });
-//});
-
-//// Other services
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-
-//builder.Services.AddScoped<AuthService>();
-//builder.Services.AddScoped<EmailService>(); // Register Email Service
-//builder.Services.AddScoped<TourPackageRepository>();
-//builder.Services.AddScoped<PlaceService>();
-
-
-
-//var app = builder.Build();
-
-//// Use CORS policy
-//app.UseCors("AllowAllOrigins");
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
-//app.MapControllers();
-
-//app.Run();
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using TourismGalle.Services;
 using TourismGalle.Models;
 using TourismGalle.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add CORS policy
+// ✅ Add CORS policy (named "AllowFrontend")
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAllOrigins",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:4200") // Angular development server
-                   .AllowAnyMethod()
-                   .AllowAnyHeader()
-                   .AllowCredentials();
-        });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Angular dev server
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials(); // Only if using cookies or auth headers
+    });
 });
 
 // Other services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<TourPackageRepository>();
-//builder.Services.AddScoped<PlaceService>();
-
-
+// builder.Services.AddScoped<PlaceService>(); // Uncomment if needed
 
 var app = builder.Build();
 
-// Use CORS policy
+// ✅ Use CORS policy here
 app.UseCors("AllowFrontend");
 
+// Swagger (only in development)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -95,7 +43,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
