1276908120f72ad36c8d1d3712f12a2c465b23a7
alokaar <subathiaswini2002@gmail.com>
2025-05-02 11:16:59 +0530

    lib 2

--- obj/Debug/net9.0/TourismGalle.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismGalle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f4ec4c7da1261ae87c142f56c7bc7c48be590b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2467410b0a0e341ed38e0ffec4dfe3a42924dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismGalle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
